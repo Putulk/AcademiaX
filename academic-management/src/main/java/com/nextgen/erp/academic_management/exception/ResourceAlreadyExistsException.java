a719a9ea8785a35b8ed018e6ebfa71ff26e9ca3e
Putul <prachipriya297@gmail.com>
2026-07-27 13:29:25 +0530

    added academic modeule and add routes in gateway

---
+++ academic-management/src/main/java/com/nextgen/erp/academic_management/exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.nextgen.erp.academic_management.exception;
+
+public class ResourceAlreadyExistsException extends RuntimeException{
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ academic-management/src/main/java/com/nextgen/erp/academic_management/exception/UserProfileNotFoundException.java
@@ -0,0 +1,8 @@
+package com.nextgen.erp.academic_management.exception;
+
+public class UserProfileNotFoundException extends RuntimeException {
+
+    public UserProfileNotFoundException(String message) {
+        super(message);
+    }
+}
